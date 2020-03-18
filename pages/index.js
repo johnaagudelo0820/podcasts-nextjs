f955563b2b45b099eca1e93e31d2c0b129c4c5df
john alexander agudelo <johnaagudelo@gmail.com>
2020-03-18 15:38:15 -0500

    add isomorphic-fetch in getInitialProps, add styles grid podcasts

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,62 @@
-export default class extends React.Component {
-  render() {
-    return <>
-      <h1>¡Hola mundo!</h1>
-      <p>Bienvenido al curso de Next.js</p>
-      <img src="/platzi-logo.png" alt="logo platzi" />
+import 'isomorphic-fetch';
 
+export default class extends React.Component {
+  static async getInitialProps() {
+    const req = await fetch('https://api.audioboom.com/channels/recommended')
+    const { body: channels } = await req.json()
+    return { channels }
+  }
+
+  render() {
+    const { channels } = this.props;
+    return <>
+      <header>Podcasts</header>
+      <div className="channels">
+        {channels.map(channel => (
+          <a className="channel" key={ channel.id }>
+            <img src={channel.urls.logo_image.original} alt={channel.title} />
+            <h2>{channel.title}</h2>
+          </a>
+        ))}
+      </div>
       <style jsx>{`
-        h1{
-          color: red;
+        header{
+          color: #fff;
+          background: #8756ca;
+          padding: 15px;
+          text-align: center;
         }
-        p {
-          color: green;
+        .channels {
+          display: grid;
+          grid-gap: 15px;
+          padding: 15px;
+          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
         }
-        img {
-          max-width: 20%;
+        a.channel {
           display: block;
-          margin: 0 auto;
+          margin-bottom: 0.5em;
+          color: #333;
+          text-decoration: none;
+        }
+        .channel img {
+          border-radius: 3px;
+          box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+          width: 100%;
+        }
+        h2 {
+          padding: 5px;
+          font-size: 0.9em;
+          font-weight: 600;
+          margin: 0;
+          text-align: center;
         }
       `}</style>
 
       <style jsx global>{`
         body {
-          background: yellow;
-          color: red;
+          margin: 0;
+          font-family: system-ui;
+          background: white;
         }
       `}</style>
     </>
