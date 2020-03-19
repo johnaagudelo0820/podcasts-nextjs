626310dbc0e53626c03c0157d9b618afbb69e381
john alexander agudelo <johnaagudelo@gmail.com>
2020-03-18 22:52:49 -0500

    refactor channel page

--- pages/channel.js
+++ pages/channel.js
@@ -1,119 +1,66 @@
 import 'isomorphic-fetch'
-import Link from 'next/link'
+import Error from 'next/error'
 import Layout from '../components/Layout'
+import PodcastList from '../components/PodcatList'
+import ChannelGrid from '../components/ChannelGrid'
 
 export default class extends React.Component {
 
-  static async getInitialProps({ query }) {
-    const { id } = query
+  static async getInitialProps({ query, res }) {
+    try {
+      const { id } = query
+  
+      const [reqChannel, reqAudios, reqSeries] = await Promise.all([
+        fetch(`https://api.audioboom.com/channels/${id}`),
+        fetch(`https://api.audioboom.com/channels/${id}/audio_clips`),
+        fetch(`https://api.audioboom.com/channels/${id}/child_channels`)
+      ])
 
-    const [reqChannel, reqAudios, reqSeries] = await Promise.all([
-      fetch(`https://api.audioboom.com/channels/${id}`),
-      fetch(`https://api.audioboom.com/channels/${id}/audio_clips`),
-      fetch(`https://api.audioboom.com/channels/${id}/child_channels`)
-    ])
-
-    const [dataChannel, dataAudios, dataSeries] = await Promise.all([
-      reqChannel.json(),
-      reqAudios.json(),
-      reqSeries.json()
-    ])
-
-    const channel = dataChannel.body.channel;
-    const audioClips = dataAudios.body.audio_clips;
-    const series = dataSeries.body.channels;
-
-    return { channel, audioClips, series }
+      if (reqChannel.status >= 400) {
+        res.statusCode = reqChannel.status
+        return { channel: null, audioClips: null, series: null, statusCode: reqChannel.status }
+      }
+  
+      const [dataChannel, dataAudios, dataSeries] = await Promise.all([
+        reqChannel.json(),
+        reqAudios.json(),
+        reqSeries.json()
+      ])
+  
+      const channel = dataChannel.body.channel;
+      const audioClips = dataAudios.body.audio_clips;
+      const series = dataSeries.body.channels;
+  
+      return { channel, audioClips, series, statusCode: 200 }
+    } catch (error) {
+      res.statusCode = 503
+      return { channel: null, audioClips: null, series: null, statusCode: 503 }
+    }
   }
 
   render() {
-    const { channel, audioClips, series } = this.props;
-    return (<Layout title="Podcasts">
-      <h1>{channel.title}</h1>
-      <h2>Ultimos podcasts</h2>
+    const { channel, audioClips, series, statusCode } = this.props;
 
-      {audioClips.map(clip => (
-        <Link href={`/podcast?id=${clip.id}`} key={clip.id}>
-          <a>{clip.title}</a>
-        </Link>
-      ))}
+    if (statusCode !== 200) {
+      return <Error statusCode={statusCode} />
+    }
 
-      <h2>Series</h2>
-      {series.map(serie => (
-        <div>{serie.title}</div>
-      ))}
+    return (<Layout title={channel.title}>
+      <div className="banner" style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }} />
+
+      { series.length > 0 &&
+        <ChannelGrid title="Series" channels={series}/>
+      }
+
+      <PodcastList title="Ultimos Podcasts" podcastList={audioClips} />
 
       <style jsx>{`
-        header {
-          color: #fff;
-          background: #8756ca;
-          padding: 15px;
-          text-align: center;
-        }
-
         .banner {
           width: 100%;
           padding-bottom: 25%;
           background-position: 50% 50%;
           background-size: cover;
           background-color: #aaa;
-        }
-
-        .channels {
-          display: grid;
-          grid-gap: 15px;
-          padding: 15px;
-          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
-        }
-        a.channel {
-          display: block;
-          margin-bottom: 0.5em;
-          color: #333;
-          text-decoration: none;
-        }
-        .channel img {
-          border-radius: 3px;
-          box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
-          width: 100%;
-        }
-        h1 {
-          font-weight: 600;
-          padding: 15px;
-        }
-        h2 {
-          padding: 5px;
-          font-size: 0.9em;
-          font-weight: 600;
-          margin: 0;
-          text-align: center;
-        }
-
-        .podcast {
-          display: block;
-          text-decoration: none;
-          color: #333;
-          padding: 15px;
-          border-bottom: 1px solid rgba(0,0,0,0.2);
-          cursor: pointer;
-        }
-        .podcast:hover {
-          color: #000;
-        }
-        .podcast h3 {
-          margin: 0;
-        }
-        .podcast .meta {
-          color: #666;
-          margin-top: 0.5em;
-          font-size: 0.8em;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        body {
-          margin: 0;
-          font-family: system-ui;
-          background: white;
         }
       `}</style>
     </Layout>
